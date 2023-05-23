4fbcecf4796b9b3d7b3dbd344b749986d40ea647
yudevlop <helpyakeen@gmail.com>
2023-05-24 00:27:39 +0530

    assignment 1 sol
    in this commit we have added assignment 1 solution of c lang

---
+++ assign1sol.c
@@ -0,0 +1,85 @@
+// Assignment 1 C language Solution By Ayush Gupta
+#include<stdio.h>
+int main()
+// Que 1 
+/*
+{
+    printf("Hello students");
+    return 0;
+}
+*/
+// Que 2
+/*
+{
+    printf("Hello\nStudents");
+    return 0;
+}
+*/
+// Que 3
+/*
+{
+    printf("\"MySirG\"");
+    return 0;
+}
+*/
+// Que 4
+/*
+{
+    printf("\\n");
+    return 0;
+}
+*/
+// Que 5
+/*
+{
+    printf("\\r");
+    return 0;
+}
+*/
+// Que 6
+/*
+{
+    printf("\"Teacher's Day\"");
+    return 0;
+}
+*/
+// Que 7
+/*
+{
+    int a,b;
+    printf ("Enter two numbers ");
+    scanf("%d %d",&a,&b);
+    printf(" Sum of %d and %d is %d",a,b,a+b);
+    return 0;
+}
+*/
+// Que 8
+/*
+{
+    int d;
+    printf("Enter the Number ");
+    scanf("%d",&d);
+    printf("Square of %d is %d",d,d*d);
+    return 0;
+}
+*/
+// Que 9
+/*
+{
+    int a,b ;
+    printf("Enter the Length and Breadth of Rectangle ");
+    scanf("%d  %d",&a,&b);
+    printf("Area of Rectangle is %d",a*b);
+    return 0;
+}
+*/
+// Que 10
+
+{
+    printf("Enter the radius of circle ");
+    float r;
+    scanf("%f",&r);
+    printf("Area of circle is %f",3.14*r*r);
+
+    return 0;
+}
